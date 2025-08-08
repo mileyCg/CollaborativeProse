e174d1e68bf7eb0718ec5bda540862435299ee6e
Yau <s0586941@htw-berlin.de>
2025-08-08 12:49:25 +0200

    mvn generated

---
+++ my_prose/src/main/java/de/htw-berlin/fb4/my-prose/App.java
@@ -0,0 +1,13 @@
+package de.htw-berlin.fb4.my-prose;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
